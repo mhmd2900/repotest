aa93f8373ec299213b758bf4b9b992a055358222
mhmd2900 <mohamedsad2900@gmail.com>
2025-09-09 12:10:52 +0300

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-// Manual square root function using Newton-Raphson method
-double manualSqrt(double num) {
-    if (num < 0) return -1; // Indicate invalid for negative input
-    double x = num;
-    double y = 1;
-    double e = 0.00001; // Set accuracy
-    while (x - y > e) {
-        x = (x + y) / 2;
-        y = num / x;
-    }
-    return x;
-}
-
 int main() {
-    double a, b, c;
-    cout << "Enter coefficient a: ";
-    cin >> a;
-    cout << "Enter coefficient b: ";
-    cin >> b;
-    cout << "Enter coefficient c: ";
-    cin >> c;
+    string s = "hello world";
+    auto it = remove(s.begin(), s.end(), 'l'); // 'l' to be removed
 
-    double discriminant = b * b - 4 * a * c;
-    cout << "Equation: " << a << "x^2 + " << b << "x + " << c << " = 0" << endl;
+    // Print the full string after remove
+    cout << "Full string after remove: " << s << endl; // shows: heo wordld
 
-    if (discriminant >= 0) {
-        double sqrtDisc = manualSqrt(discriminant);
-        double x1 = (-b + sqrtDisc) / (2 * a);
-        double x2 = (-b - sqrtDisc) / (2 * a);
-        cout << "Root 1: " << x1 << endl;
-        cout << "Root 2: " << x2 << endl;
-    } else {
-        double sqrtDisc = manualSqrt(-discriminant);
-        double realPart = -b / (2 * a);
-        double imagPart = sqrtDisc / (2 * a);
-        cout << "Complex Root 1: " << realPart << " + " << imagPart << "i" << endl;
-        cout << "Complex Root 2: " << realPart << " - " << imagPart << "i" << endl;
+    // Print only the valid part (up to the returned iterator)
+    cout << "Logical string after remove: ";
+    for (auto p = s.begin(); p != it; ++p) {
+        cout << *p; // shows: heo word
     }
+    cout << endl;
+
     return 0;
 }
-//
-cin.ignore(numeric_limits<streamsize>::max(), '\n');
-cin >> test;
-
-
